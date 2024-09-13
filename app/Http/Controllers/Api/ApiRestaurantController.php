a597252ad459c5c084018d98eef2ac4a5cfb1a69
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-13 16:26:19 +0200

    [UPD] API for Types Restaurants and Restaurant-Details (dishes) created

---
+++ app/Http/Controllers/Api/ApiRestaurantController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class ApiRestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function apiIndex(Request $request)
+    {
+        $typeFilters = $request->query('types');
+
+        // Crea la query di base con la relazione 'types'
+        $query = Restaurant::with('types');
+
+        // Filtra per tipi, se il filtro è presente
+        if ($typeFilters) {
+            $typesArray = explode(',', $typeFilters);
+            $query->whereHas('types', function ($q) use ($typesArray) {
+                $q->whereIn('name', $typesArray);
+            });
+        }
+
+        // Esegui la query e ottieni i ristoranti
+        $restaurants = $query->get();
+
+        return response()->json($restaurants);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($slug)
+    {
+        // Ottieni il ristorante in base allo slug e carica i piatti
+        $restaurant = Restaurant::with('dishes')->where('slug', $slug)->first();
+
+        // Controlla se il ristorante esiste
+        if (!$restaurant) {
+            return response()->json(['message' => 'Ristorante non trovato'], 404);
+        }
+
+        // Restituisci il ristorante con piatti e tipi in formato JSON
+        return response()->json($restaurant);
+    }
+
+
+    public function getTypes()
+    {
+        $types = Type::all();
+        return response()->json($types);
+    }
+}
--- app/Http/Controllers/User/RestaurantController.php
+++ app/Http/Controllers/User/RestaurantController.php
@@ -171,4 +171,17 @@ class RestaurantController extends Controller
 
         return redirect()->route('user.restaurants.index')->with('message', 'Ristorante ' . $restaurant_name . ' eliminato con successo');
     }
+
+
+
+    // //(LM) metodo apiIndex per restituire i ristoranti in formato JSON
+    // public function apiIndex()
+    // {
+
+    //     //si ottengono tutti i ristoranti
+    //     $restaurants = Restaurant::with('types')->get();
+
+    //     //restituzione dati in formato json
+    //     return response()->json($restaurants);
+    // }
 }
