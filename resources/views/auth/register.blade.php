4cf29da76aa708f8830d2060864d03a21257beda
rinox3005 <peppe.rinox@gmail.com>
2024-09-25 18:37:29 +0200

    [UPD] restyled login and register pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
+@section('title')
+    Deliveboo | Partner Hub - Register
+@endsection
+
 @section('content')
     <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+                    <div class="card-header fs-4">{{ __('Registrazione Nuovo Ristoratore') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
@@ -74,7 +78,8 @@
 
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit"
+                                        class="py-2 px-4 bg-custom-primary fs-6 custom-btn me-2 border border-none mt-2 text-white">
                                         {{ __('Register') }}
                                     </button>
                                 </div>
